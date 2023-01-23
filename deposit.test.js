702fdc32fa09c8456c5ea95cef75352a1d8514b6
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-23 17:51:36 +0000

    Changed integer values to floats and wrote tests to check withdraw and deposit can handle more specific values

--- deposit.test.js
+++ deposit.test.js
@@ -3,29 +3,38 @@ const Deposit = require('./Deposit');
 describe('Deposit', () => {
   it ('adds funds to empty bank balance', () => {
     const deposit = new Deposit();
-    deposit.balance = 0;
+    deposit.balance = 0.00;
 
-    deposit.depositFunds(1000);
+    deposit.depositFunds(1000.00);
 
-    expect(deposit.balance).toBe(1000);
+    expect(deposit.balance).toBe(1000.00);
   });
 
   it ('adds further funds to existing bank balance', () => {
     const deposit = new Deposit();
-    deposit.balance = 2255;
+    deposit.balance = 2255.00;
 
-    deposit.depositFunds(745);
+    deposit.depositFunds(745.00);
 
-    expect(deposit.balance).toBe(3000);
+    expect(deposit.balance).toBe(3000.00);
   });
+
+  it ('can deposit more specific values', () => {
+    const deposit = new Deposit ();
+    deposit.balance = 5124.23
+
+    deposit.depositFunds(532.51);
+
+    expect(deposit.balance).toBe(5656.74);
+  })
 
   it ('can be called multiple times in one go', () => {
     const deposit = new Deposit();
-    deposit.balance = 1000;
+    deposit.balance = 1000.00;
 
-    deposit.depositFunds(450);
-    deposit.depositFunds(550);
+    deposit.depositFunds(450.00);
+    deposit.depositFunds(550.00);
 
-    expect(deposit.balance).toBe(2000);
+    expect(deposit.balance).toBe(2000.00);
   });
 });
--- withdraw.test.js
+++ withdraw.test.js
@@ -12,20 +12,29 @@ describe('Withdraw', () => {
 
   it ('can be called multiple times in one go', () => {
     const withdraw = new Withdraw();
-    withdraw.balance = 10000;
+    withdraw.balance = 10000.00;
 
-    withdraw.withdrawFunds(7000);
-    withdraw.withdrawFunds(2000);
+    withdraw.withdrawFunds(7000.00);
+    withdraw.withdrawFunds(2000.00);
 
-    expect(withdraw.balance).toBe(1000);
+    expect(withdraw.balance).toBe(1000.00);
   });
+
+  it ('can withdraw more specific values', () => {
+    const withdraw = new Withdraw();
+    withdraw.balance = 7575.75;
+
+    withdraw.withdrawFunds(243.62);
+
+    expect(withdraw.balance).toBe(7332.13);
+  })
 
   it ('throws an error if withdrawing from empty account', () => {
     const withdraw = new Withdraw();
-    withdraw.balance = 0;
+    withdraw.balance = 0.00;
 
     try {
-      (withdraw.withdrawFunds(500));
+      (withdraw.withdrawFunds(500.00));
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       expect(error.message).toBe("Not enough funds in account!")
@@ -34,10 +43,10 @@ describe('Withdraw', () => {
 
   it ('throws an error if withdrawl amount is greater than balance', () => {
     const withdraw = new Withdraw();
-    withdraw.balance = 5550;
+    withdraw.balance = 5550.00;
 
     try {
-      (withdraw.withdrawFunds(7500));
+      (withdraw.withdrawFunds(7500.00));
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       expect(error.message).toBe("Not enough funds in account!")
