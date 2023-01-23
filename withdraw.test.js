6c0b4edfa17dc75807c74ea29b011376497f51ab
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-23 17:16:36 +0000

    Passed second test in withdraw.test.js

--- withdraw.test.js
+++ withdraw.test.js
@@ -10,15 +10,15 @@ describe('Withdraw', () => {
     expect(withdraw.balance).toBe(4000);
   });
 
-  // it ('can be called multiple times in one go', () => {
-  //   const withdraw = new Withdraw();
-  //   withdraw.balance = 10000;
+  it ('can be called multiple times in one go', () => {
+    const withdraw = new Withdraw();
+    withdraw.balance = 10000;
 
-  //   withdraw.withdrawFunds(7000);
-  //   withdraw.withdrawFunds(2000);
+    withdraw.withdrawFunds(7000);
+    withdraw.withdrawFunds(2000);
 
-  //   expect(withdraw.balance).toBe(1000);
-  // });
+    expect(withdraw.balance).toBe(1000);
+  });
 
   // it ('throws an error if withdrawl amount is greater than balance', () => {
 
