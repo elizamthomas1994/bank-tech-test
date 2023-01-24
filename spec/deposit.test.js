50625c4ff4630223800e6e206a07535a067efcb5
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-24 17:53:28 +0000

    Rewrote deposit jest tests and class functionality to take deposit amount as argument

--- spec/deposit.test.js
+++ spec/deposit.test.js
@@ -2,39 +2,26 @@ const Deposit = require('../lib/deposit');
 
 describe('Deposit', () => {
   it ('adds funds to empty bank balance', () => {
-    const deposit = new Deposit();
-    deposit.balance = 0.00;
+    const deposit = new Deposit(0.00, 1000.00);
 
-    deposit.depositFunds(1000.00);
+    deposit.depositFunds();
 
     expect(deposit.balance).toBe(1000.00);
   });
 
   it ('adds further funds to existing bank balance', () => {
-    const deposit = new Deposit();
-    deposit.balance = 2255.00;
+    const deposit = new Deposit(2255.00, 745.00);
 
-    deposit.depositFunds(745.00);
+    deposit.depositFunds();
 
     expect(deposit.balance).toBe(3000.00);
   });
 
   it ('can deposit more specific values', () => {
-    const deposit = new Deposit ();
-    deposit.balance = 5124.23
+    const deposit = new Deposit (5124.23, 532.51);
 
-    deposit.depositFunds(532.51);
+    deposit.depositFunds();
 
     expect(deposit.balance).toBe(5656.74);
   })
-
-  it ('can be called multiple times in one go', () => {
-    const deposit = new Deposit();
-    deposit.balance = 1000.00;
-
-    deposit.depositFunds(450.00);
-    deposit.depositFunds(550.00);
-
-    expect(deposit.balance).toBe(2000.00);
-  });
 });
