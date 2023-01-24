dc34d64d0c9c8b1cee6e65dbb6e8b204cf0506c2
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-24 17:14:02 +0000

    Implemented displayStatement function in account.js and tested this

--- spec/account.test.js
+++ spec/account.test.js
@@ -6,14 +6,14 @@ describe('Account', () => {
 
     expect(account.checkBalance()).toBe(0.00);
   });
-
+  
   it ('records all account deposits', () => {
     const account = new Account(1250.00);
     
     account.deposit(500.25);
 
     expect(account.recordedTransactions).toEqual([['24/01/2023', 500.25, ' ', 1750.25]])
-  })
+  });
 
   it ('records all account withdrawals', () => {
     const account = new Account(5000.00);
@@ -21,5 +21,11 @@ describe('Account', () => {
     account.withdraw(499.25);
 
     expect(account.recordedTransactions).toEqual([['24/01/2023', 499.25, ' ', 4500.75]])
-  })
+  });
+
+  it ('outputs header if no transactions made', () => {
+    const account = new Account(0.00);
+
+    expect(account.displayStatement()).toBe("Date || Credit || Debit || Balance");
+  });
 });
