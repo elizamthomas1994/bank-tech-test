c6785a0412861c44c6ad9783be28b2e24c16fa87
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-24 17:57:05 +0000

    Rewrote withdraw tests and functionality to take amount withdrawn as a class argument

--- spec/withdraw.test.js
+++ spec/withdraw.test.js
@@ -2,39 +2,26 @@ const Withdraw = require('../lib/withdraw');
 
 describe('Withdraw', () => {
   it ('deducts funds from the bank balance', () => {
-    const withdraw = new Withdraw();
-    withdraw.balance = 5000;
+    const withdraw = new Withdraw(5000, 1000);
 
-    withdraw.withdrawFunds(1000);
+    withdraw.withdrawFunds();
 
     expect(withdraw.balance).toBe(4000);
   });
 
-  it ('can be called multiple times in one go', () => {
-    const withdraw = new Withdraw();
-    withdraw.balance = 10000.00;
-
-    withdraw.withdrawFunds(7000.00);
-    withdraw.withdrawFunds(2000.00);
-
-    expect(withdraw.balance).toBe(1000.00);
-  });
-
   it ('can withdraw more specific values', () => {
-    const withdraw = new Withdraw();
-    withdraw.balance = 7575.75;
+    const withdraw = new Withdraw(7575.75, 243.62);
 
-    withdraw.withdrawFunds(243.62);
+    withdraw.withdrawFunds();
 
     expect(withdraw.balance).toBe(7332.13);
   })
 
   it ('throws an error if withdrawing from empty account', () => {
-    const withdraw = new Withdraw();
-    withdraw.balance = 0.00;
+    const withdraw = new Withdraw(0.00, 500.00);
 
     try {
-      (withdraw.withdrawFunds(500.00));
+      (withdraw.withdrawFunds());
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       expect(error.message).toBe("Not enough funds in account!")
@@ -42,11 +29,10 @@ describe('Withdraw', () => {
   })
 
   it ('throws an error if withdrawl amount is greater than balance', () => {
-    const withdraw = new Withdraw();
-    withdraw.balance = 5550.00;
+    const withdraw = new Withdraw(5500.00, 7500.00);
 
     try {
-      (withdraw.withdrawFunds(7500.00));
+      (withdraw.withdrawFunds());
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       expect(error.message).toBe("Not enough funds in account!")
