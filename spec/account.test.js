708827e66a3df7f53f48f9c93d8050d26d012cbd
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-24 16:46:06 +0000

    Successfully wrote and passed test for deposit function in account.js

--- spec/account.test.js
+++ spec/account.test.js
@@ -6,4 +6,12 @@ describe('Account', () => {
 
     expect(account.accountBalance()).toBe(0.00);
   });
+
+  it ('records all account deposits', () => {
+    const account = new Account(1250.00);
+    
+    account.deposit(500.25);
+
+    expect(account.recordedTransactions).toEqual([['24/01/2023', 500.25, ' ', 1750.25]])
+  })
 });
